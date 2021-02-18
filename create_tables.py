9e20722678c6e6fede91129ed45a1f3a4e8e9e72
Anita Vero <alv34@cam.ac.uk>
2021-02-18 10:23:22 +0100

    Update cluster files and insert data.

--- create_tables.py
+++ create_tables.py
@@ -12,13 +12,8 @@ if __name__ == '__main__':
 
     for clfile in cf['clfiles']:
         with open(os.path.join(cf['datapath'], clfile), 'r') as f:
-            cl_dict = json.load(f)
-
-        # TODO: load new cluster files, which don't need to be converted
-        clusters = defaultdict(list)
-        for w, c in cl_dict.items():
-            clusters[int(c)].append(w)
+            clusters = json.load(f)
 
         embid = clfile.split('.')[0]
-        for cl, words in clusters.items():
+        for cl, words in clusters:
             Cluster.create(cluster=f'{embid}_{cl}', words=', '.join(words))
